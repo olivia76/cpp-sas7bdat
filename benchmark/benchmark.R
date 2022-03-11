9fa0826b5747b5a1fdfd718de6118046eb758b25
Olivia Quinet <olivia.quinet@cluepoints.com>
2022-03-11 15:17:00 +0100

    fix code

--- benchmark/benchmark.R
+++ benchmark/benchmark.R
@@ -7,25 +7,25 @@ print(CPPSAS7BDAT::getVersion());
 args <- commandArgs(trailingOnly = TRUE)
 
 fct1 <- function(sink) {
-     r <- CPPSAS7BDAT::Reader(args[1], sink);
+     r <- CPPSAS7BDAT::sas_reader(args[1], sink);
      r$read_all();
 }
 
 benchmark(#"Sink" = {
-		 print("Sink")
-		 sink <- Sink$new();
-		 fct1(sink)
-		 #print(sink$df)
-		 },
+	  #	 print("Sink")
+	  #	 sink <- SinkByRow$new();
+	  #	 fct1(sink)
+	  #	 #print(sink$df)
+	  #	 },
           "SinkChunk" = {
 		 print("SinkByChunk")
-		 sink <- SinkChunk$new(10000);
+		 sink <- SinkByChunk$new(10000);
 		 fct1(sink)
 		 #print(sink$df)
 		 },
           "SinkData" = {
 		 print("SinkWholeData")
-		 sink <- SinkData$new();
+		 sink <- SinkWholeData$new();
 		 fct1(sink)
 		 #print(sink$df)
 		 },
