1cd22561a13ee3df6bcec0b057f928de2014b81f
Olivia Quinet <olivia.quinet@cluepoints.com>
2022-05-03 10:52:30 +0200

    added tests for memory allocation

--- src/memory.hpp
+++ src/memory.hpp
@@ -9,6 +9,7 @@
 #ifndef _CPP_SAS7BDAT_SRC_MEMORY_HPP_
 #define _CPP_SAS7BDAT_SRC_MEMORY_HPP_
 
+#include "debug.hpp"
 #include "exceptions.hpp"
 #include <cstdint>
 #include <fmt/core.h>
@@ -22,9 +23,7 @@ namespace MEMORY {
 constexpr std::align_val_t al{alignof(uint64_t)};
 inline uint8_t *aligned_alloc(const size_t _size) {
   D(spdlog::info(fmt::format("INTERNAL::MEMORY::aligned_alloc({})", _size)));
-  void *pv = operator new(_size, al);
-  if (!pv)
-    EXCEPTION::cannot_allocate_memory();
+  void *pv = operator new(_size, al); // Throws std::bad_alloc
   return reinterpret_cast<uint8_t *>(pv);
 }
 inline void aligned_free(uint8_t *_buffer) {
