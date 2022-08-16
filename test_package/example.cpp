2ef86536393ac75a71c4cb7fc37cf79174d71a48
Olivia Quinet <olivia.quinet@cluepoints.com>
2022-08-16 10:56:07 +0200

    fix in test_package/example.cpp

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,10 +1,10 @@
 #include <cppsas7bdat/reader.hpp>
 #include <cppsas7bdat/version.hpp>
-#include <cppsas7bdat/datasource_ifstream.hpp>
-#include <cppsas7bdat/datasink_null.hpp>
+#include <cppsas7bdat/source/ifstream.hpp>
+#include <cppsas7bdat/sink/null.hpp>
 
 int main() {
   cppsas7bdat::getVersion();
-  //auto reader = cppsas7bdat::Reader(cppsas7bdat::datasource::ifstream("file.sas7bdat"), cppsas7bdat::datasink::null());
+  //auto reader = cppsas7bdat::Reader(cppsas7bdat::datasource::ifstream("../../file.sas7bdat"), cppsas7bdat::datasink::null());
   return 0;
 }
